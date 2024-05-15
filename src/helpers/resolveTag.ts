623405681ceebcc27ff81be7207cf0a25ebebe40
ryancundiff <ryancundif2006@gmail.com>
2024-05-15 17:32:43 -0500

    style: remove space

--- src/helpers/resolveTag.ts
+++ src/helpers/resolveTag.ts
@@ -1,7 +1,7 @@
 /**
  * Resolves a tag to a valid tag format.
  * @param tag Tag to resolve.
- */
+*/
 export function resolveTag (tag: string) {
   // Credit: clashperk/clashofclans.js
   return `#${tag.toUpperCase().replace(/[#]/g, '').replace(/[\s]/g, '').replace(/[O]/g, '0')}`
